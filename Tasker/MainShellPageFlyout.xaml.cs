e8374d2cd582a241e1427a007e75fbafd1e89026
husamaldeen0000@gmail.com <husamaldeen0000@gmail.com>
2021-10-16 09:02:50 +0200

    update

--- Tasker/MainShellPageFlyout.xaml.cs
+++ Tasker/MainShellPageFlyout.xaml.cs
@@ -35,27 +35,37 @@ namespace Tasker
                         Title = "All Products",
                         TargetType=typeof(Views.ProductsPage)
                     },
+
                     new MainShellPageFlyoutMenuItem
                     {
                         Id = 1,
-                        Tag=new ViewModels.ProductViewModel(new Category()   {
-                         Id=1})
-                    , Title = "Boots",TargetType=typeof(Views.ProductsPage)
+                        Tag=new ViewModels.ProductViewModel(new Category() {Id=1}),
+                        Title = "Boots",
+                        TargetType=typeof(Views.ProductsPage)
+                  
                     },
-                    new MainShellPageFlyoutMenuItem { Id = 2,
-                        Tag=new ViewModels.ProductViewModel(new Category()   {
-                          Id=2})
-                    , Title = "Shirts",TargetType=typeof(Views.ProductsPage)
+
+                    new MainShellPageFlyoutMenuItem 
+                    { 
+                        Id = 2,
+                        Tag=new ViewModels.ProductViewModel(new Category() {Id=2}),
+                        Title = "Shirts",
+                        TargetType=typeof(Views.ProductsPage)
                     },
-                     new MainShellPageFlyoutMenuItem { Id = 3,
-                        Tag=new ViewModels.ProductViewModel(new Category()   {
-                          Id=3})
-                    , Title = "Jacket",TargetType=typeof(Views.ProductsPage)
+
+                     new MainShellPageFlyoutMenuItem
+                     { 
+                         Id = 3,
+                         Tag=new ViewModels.ProductViewModel(new Category(){ Id=3}),
+                         Title = "Jacket",
+                         TargetType=typeof(Views.ProductsPage)
                     },
-                     new MainShellPageFlyoutMenuItem { Id = 4,
-                        Tag=new ViewModels.ProductViewModel(new Category()   {
-                         Id=4})
-                    , Title = "Cap",TargetType=typeof(Views.ProductsPage)
+
+                     new MainShellPageFlyoutMenuItem 
+                     { 
+                         Id = 4,
+                         Tag=new ViewModels.ProductViewModel(new Category(){Id=4}),
+                         Title = "Cap",TargetType=typeof(Views.ProductsPage)
                     }
                 });
             }
